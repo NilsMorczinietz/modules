6e20ef0a7cfb91b8f8f57f17f7540d1338648aaa
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2026-02-25 12:30:58 +0000

    feat: add detailed descriptions for exam types in the accordion component

--- src/routes/exam-types/+page.svelte
+++ src/routes/exam-types/+page.svelte
@@ -4,11 +4,13 @@
   const examTypes = [
     {
       title: 'Zugangskolloquium',
-      content: ''
+      content:
+        'Ein Zugangskolloquium dient der Feststellung, ob die Studierenden die versuchsspezifischen Voraussetzungen erfüllen, eine definierte laborpraktische Aufgabe nach wissenschaftlichen und fachpraktischen Methoden selbstständig und sicher bearbeiten zu können.'
     },
     {
       title: 'Präparat',
-      content: ''
+      content:
+        'Ein Präparat ist das materielle Produkt einer Arbeitsleistung, das hinsichtlich seiner Qualität und Quantität zuvor festgelegten Kriterien genügt. Es dient der Feststellung, ob der Prüfling befähigt ist, innerhalb vorgegebener Fristen eine Aufgabe mit dem Ziel der Herstellung eines Produktes nach wissenschaftlichen und fachpraktischen Methoden selbstständig zu bearbeiten.'
     },
     {
       title: 'Mündlicher Beitrag',
@@ -22,7 +24,8 @@
     },
     {
       title: 'Projektarbeit',
-      content: ''
+      content:
+        'Die Projektarbeit ist eine Prüfungsleistung, die in der selbstständigen Bearbeitung einer spezifischen Fragestellung unter Anleitung mit wissenschaftlicher Methodik und einer Dokumentation der Ergebnisse besteht.'
     },
     {
       title: 'Lernportfolio',
@@ -31,23 +34,28 @@
     },
     {
       title: 'Praktikumsbericht',
-      content: ''
+      content:
+        'Ein Praktikumsbericht (z. B. Versuchsprotokoll) dient der Feststellung, ob die Studierenden befähigt sind, innerhalb einer vorgegebenen Frist eine laborpraktische Aufgabe selbstständig sowohl praktisch zu bearbeiten als auch Bearbeitungsprozess und Ergebnis schriftlich zu dokumentieren, zu bewerten und zu reflektieren. Praktikumsberichte können auch in Form einer Gruppenarbeit zur Prüfung zugelassen werden. Die Bewertung des Praktikumsberichts ist den Studierenden spätestens sechs Wochen nach Abgabe des Berichts bekannt zu geben.'
     },
     {
       title: 'Open-Book-Ausarbeitung',
-      content: ''
+      content:
+        'Die Open-Book-Ausarbeitung oder -Arbeit (OBA) ist eine Kurzhausarbeit und damit eine unbeaufsichtigte schriftliche oder elektronische Prüfung. Sie zeichnet sich dadurch aus, dass gemäß Hilfsmittelerklärung der Prüferin bzw. des Prüfers in der Regel alle Hilfsmittel zugelassen sind. Auf die Sicherung guter wissenschaftlicher Praxis durch ordnungsgemäßes Zitieren etc. und das Erfordernis der Eigenständigkeit der Erbringung jedweder Prüfungsleistung wird besonders hingewiesen.'
     },
     {
       title: 'Testat/Zwischentestat',
-      content: ''
+      content:
+        'Mit einem Testat/Zwischentestat wird bescheinigt, dass die oder der Studierende eine Stu-dienarbeit (z.B. Entwurf) im geforderten Umfang erstellt hat. Der zu erbringende Leistungs-umfang sowie die geforderten Inhalte und Anforderungen ergeben sich aus der jeweiligen Modulbeschreibung im Modulhandbuch sowie aus der Aufgabenstellung.'
     },
     {
       title: 'Performanzprüfung',
-      content: ''
+      content:
+        'Im Rahmen einer Performanzprüfung werden realitätsnahe, typische Handlungssituationen simuliert. Die Studierenden werden hierzu mit einer oder mehreren Aufgabenstellungen konfrontiert, wie sie in ihrem späteren Berufsfeld tatsächlich vorkommen (können). Die Studierenden müssen diese Aufgabenstellung – nach Maßgabe der konkreten Ausgestaltung in dem jeweiligen Modul – allein oder in der Rolle eines Mitglieds einer mit den jeweiligen Aufgaben betrauten Gruppe in eigener Verantwortung lösen. Wie sorgfältig die Aufgabenstellung analysiert und welcher Lösungsweg eingeschlagen wird, welche Methoden und Instrumente ausgewählt und eingesetzt werden und wie die Studierenden die eigenen Aktivitäten sowie die Zusammenarbeit mit den anderen Gruppenmitgliedern ausgestalten, organisieren, koordinieren und dokumentieren (Projektmanagement), bestimmen die Studierenden analog zur beruflichen Praxis weitgehend selbst; dies wird bewertet (Performanz).'
     },
     {
       title: 'Rollenspiel',
-      content: ''
+      content:
+        'Ein Rollenspiel (auch Planspiel) dient der Feststellung, ob die Studierenden befähigt sind, innerhalb einer vorgegebenen Zeitspanne in einer praxisnahen oder praxisanalogen Situation bzw. Simulation Aufgaben mit wissenschaftlichen Methoden und unter Einsatz von Kommunikations- und Kooperationstechniken in der Regel im Diskurs mit weiteren handelnden, realen oder virtuellen Personen zu lösen. Die Bewertung ist den Studierenden nach Abschluss des Rollenspiels bekannt zu geben.'
     },
     {
       title: 'E-Prüfung',
@@ -56,7 +64,8 @@
     },
     {
       title: 'Klausurarbeit',
-      content: ''
+      content:
+        'Eine Klausurarbeit ist eine schriftliche Prüfung, in der die oder der Studierende unter Aufsicht und innerhalb einer begrenzten Zeit mit zugelassenen Hilfsmitteln nachweist, dass sie oder er Themen oder Fragestellungen eines Moduls mit den geläufigen wissenschaftlichen Methoden des jeweiligen Fachgebiets erkennen und sachgerecht lösen kann. Die Klausur findet in der Regel als Präsenzprüfung statt, kann jedoch auch elektronisch durchgeführt werden (eKlausur), einschließlich einer elektronischen Fernklausur. Elektronische Prüfungen werden wie schriftliche Prüfungen behandelt. Dabei wird sichergestellt, dass die Prüfungsbedingungen ordnungsgemäß überwacht, die Identität der Studierenden festgestellt und Täuschungen möglichst verhindert werden, unter Wahrung von Persönlichkeitsschutz und Privatsphäre. Die Prüfungsaufgabe wird in der Regel von einer Prüferin oder einem Prüfer gestellt und bewertet. In fachlich begründeten Fällen können mehrere Prüfende beteiligt sein; die Gewichtung der jeweiligen Anteile wird vorab festgelegt.'
     },
     {
       title: 'Schriftliche Prüfung im Antwortwahlverfahren (Multiple Choice)',
@@ -85,7 +94,7 @@
         </p>
       </div>
 
-      <Accordion.Root type="single" class="w-full sm:max-w-[70%]">
+      <Accordion.Root type="single" class="w-full sm:max-w-[65%]">
         {#each sortedExamTypes as exam}
           <Accordion.Item disabled={!exam.content}>
             <Accordion.Trigger
